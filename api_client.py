154474972ec6592cf73bab2b9ea6ee1dd6ec2ecf
Dusan Stojanovic <dusanss@oipinsurtech.com>
2026-06-03 13:54:28 +0200

    Api excercisetask-3.1-api-interaction finished v2

---
+++ api_client.py
@@ -0,0 +1,26 @@
+import requests
+
+
+def fetch_and_display_users(num_users):
+    try:
+
+        url = "https://jsonplaceholder.typicode.com/users"
+        response = requests.get(url)
+        users = response.json()
+
+        for user in users[:num_users]:
+            name = user['name']
+            email = user['email']
+            city = user['address']['city']
+            print(f"Name: {name} | Email: {email} | City: {city}")
+        
+    except requests.RequestException:   
+        print("Network issues")
+        return None
+    except (KeyError, TypeError):
+        print("The API returned an unexpected structure.")
+        return None
+
+
+fetch_and_display_users(4)
+fetch_and_display_users(16)
